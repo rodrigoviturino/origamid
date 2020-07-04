743fec409814fd7fd3d280a4d6b74ac55761ed16
Rodrigo Viturino <viturino.souza1@gmail.com>
2020-07-04 12:09:02 -0300

    modulo efeitos no dom - clickoutside

---
+++ Javascript ES6/05 - efeitos-no-dom/clickoutside/js/script.js
@@ -0,0 +1,18 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initScrollAnimacao from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+
+initScrollSuave();
+initScrollAnimacao();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+
+// const h1 = document.querySelector("h1");
+// console.log(Object.prototype.toString.call(h1));
