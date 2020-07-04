f47222bec46c63de30c846721c86f9272562cd3a
Rodrigo Viturino <viturino.souza1@gmail.com>
2020-07-04 18:40:41 -0300

    Desenvolvimento de adicionar menu mobile com efeito de click block fora do menu

---
+++ Javascript ES6/05 - efeitos-no-dom/menu_responsivo/js/script.js
@@ -0,0 +1,20 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initScrollAnimacao from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+
+initScrollSuave();
+initScrollAnimacao();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+
+// const h1 = document.querySelector("h1");
+// console.log(Object.prototype.toString.call(h1));
