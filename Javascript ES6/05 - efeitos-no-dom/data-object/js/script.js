955fadbe2ce982cd61ea548f31c589407f7132b4
Rodrigo Viturino <Viturino.souza1@gmail.com>
2020-07-13 19:20:48 -0300

    Aula Data Object - concluido - verificando como trabalhar com data e horas

---
+++ Javascript ES6/05 - efeitos-no-dom/data-object/js/script.js
@@ -0,0 +1,24 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initScrollAnimacao from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
+
+initScrollSuave();
+initScrollAnimacao();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
+
+// const h1 = document.querySelector("h1");
+// console.log(Object.prototype.toString.call(h1));
