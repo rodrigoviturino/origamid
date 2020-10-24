f0f96380dd3719d5fed45f2493536fea9fb63e3e
Rodrigo Viturino <viturino.souza1@gmail.com>
2020-10-24 17:21:45 -0300

    Projeto Techno - Metodos criado de Router, alerta,produto de observação no watch para ativar o HASH de ao clicar no item, modificar a rota e o title

--- Vue/projeto-tecno/techno/app.js
+++ Vue/projeto-tecno/techno/app.js
@@ -3,7 +3,9 @@ const vm = new Vue({
   data: {
     produtos: [],
     produto: false,
-    carrinho: []
+    carrinho: [],
+    mensagemAlerta: "Item Adicionado",
+    alertaAtivo: false,
   },
   filters: {
     precoBR(valor) {
@@ -48,10 +50,30 @@ const vm = new Vue({
     adicionarItem(){
       this.produto.estoque--;
       const {id, nome, preco} = this.produto;
-      this.carrinho.push({id, nome, preco})
+      this.carrinho.push({id, nome, preco});
+      this.alerta(`${nome} foi adicionado.`);
+
     },
     removerItem(index){
       this.carrinho.splice(index, 1);
+    },
+    checarLocalStorage(){
+      if(window.localStorage.carrinho){
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+      }
+    },
+    alerta(mensagem){
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() => {
+        this.alertaAtivo = false;
+      },1500)
+    },
+    router(){
+      const hash = document.location.hash;
+      if(hash){
+        this.fetchProduto(hash.replace("#", ""));
+      }
     }
   },
   // Método Computed é reativo
@@ -66,7 +88,19 @@ const vm = new Vue({
       return total;
     }
   },
+  watch: {
+    produto(){
+      document.title = this.produto.nome || "Techno";
+      let hash = this.produto.id || "";
+      history.pushState(null, null, `#${hash}`)
+    },
+    carrinho(){
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
+    }
+  },
   created() {
     this.fetchProdutos();
+    this.router();
+    this.checarLocalStorage();
   },
 });
